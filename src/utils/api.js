6cbcfb4de4ad6f028fa25c4dc425a7fbd46aa7b3
Filla Ramadhan <fillar71@gmail.com>
2025-10-29 23:37:07 +0700

    update src/utils/api.js

---
+++ src/utils/api.js
@@ -0,0 +1,19 @@
+// src/utils/api.js
+import axios from "axios";
+
+export const BASE_URL = "https://<nama-app>.up.railway.app";
+
+export const api = axios.create({
+  baseURL: BASE_URL,
+  headers: { "Content-Type": "application/json" }
+});
+
+export async function generateVideoScript(config) {
+  const res = await api.post("/api/generate", config);
+  return res.data.timeline;
+}
+
+export async function saveProject(project) {
+  const res = await api.post("/api/saveProject", project);
+  return res.data;
+}
